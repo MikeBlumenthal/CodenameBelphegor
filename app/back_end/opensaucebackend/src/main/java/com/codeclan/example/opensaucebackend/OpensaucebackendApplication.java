da4ac5a0f17289ed201f45ead5ec540551fec76b
Michael Blumenthal <bloomer25@googlemail.com>
2018-11-02 15:29:31 +0000

    initialise react in frontend and spring in backend

---
+++ app/back_end/opensaucebackend/src/main/java/com/codeclan/example/opensaucebackend/OpensaucebackendApplication.java
@@ -0,0 +1,12 @@
+package com.codeclan.example.opensaucebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensaucebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpensaucebackendApplication.class, args);
+	}
+}
